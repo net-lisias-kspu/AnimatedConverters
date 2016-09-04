61b396ed3eb8f9bf0a1181fe0cf1015ed119cb7a
Allis Tauri <allista@gmail.com>
2016-09-04 21:33:03 +0300

    Moved PartModule.ConfigurationInvalid extension to AT_Utils.

--- Extensions.cs
+++ Extensions.cs
@@ -9,16 +9,6 @@ namespace AT_Utils
 {
 	public static class AnimatedConvertersExtensions
 	{
-		public static void ConfigurationInvalid(this PartModule pm, string msg, params object[] args)
-		{
-			Utils.Message(6, "WARNING: {0}.\n" +
-			              "Configuration of \"{1}\" is INVALID.", 
-			              string.Format(msg, args), 
-			              pm.Title());
-			pm.enabled = pm.isEnabled = false;
-			return;
-		}
-
 		public static PartResourceDefinition GetResourceDef(this PartModule pm, string name)
 		{
 			var res = PartResourceLibrary.Instance.GetDefinition(name);
